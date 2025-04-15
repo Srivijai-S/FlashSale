c2d511a327ef3f3351ff671cbb1f95d2ddabf434
Srivijai <srivijai@ysquaretechnologu.com>
2025-04-15 20:27:11 +0530

    fix-viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This ensures env vars are available
+    "process.env": {
+      VITE_AIRTABLE_API_KEY: JSON.stringify(process.env.VITE_AIRTABLE_API_KEY),
+    },
+  },
   build: {
     outDir: "dist",
   },
